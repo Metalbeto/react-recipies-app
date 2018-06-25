0b63f1456fe090d63d99aac8ba5226484eb5299b
Metalbeto <alberto.ortiz.36@hotmail.com>
2018-06-24 21:46:02 -0500

    Update appsync.js

--- src/appsync.js
+++ src/appsync.js
@@ -1,6 +1,1 @@
-export default {
-    "graphqlEndpoint": "https://rzbyb7vi6rdudc6mpen52ch5vi.appsync-api.us-east-1.amazonaws.com/graphql",
-    "region": "us-east-1",
-    "authenticationType": "API_KEY",
-    "apiKey": "da2-ok5taxig65gbjevxt4xbntcyfa"
-}
+// Your own configuration file AppSync.js downloaded from your AppSync API.
